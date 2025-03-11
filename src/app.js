1ca27d80019460719da1bc2f55cb7be24ab3ea1b
vyshnavigopireddy <gopireddyvyshnavi7@gmail.com>
2025-03-12 00:28:00 +0530

    Updated app.js with new changes

--- src/app.js
+++ src/app.js
@@ -13,7 +13,7 @@ app.get("/", (req, res) => {
     res.send("Welcome to the School Management API!");
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 10000;
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
 });
